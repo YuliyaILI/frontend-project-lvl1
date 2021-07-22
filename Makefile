9f524e78b86fb81caa769569e7a7c4a5dacab873
Yuliya Ilina <shubinayv@ya.ru>
2021-07-23 00:11:23 +0300

    step 5 proverka

--- Makefile
+++ Makefile
@@ -1,8 +1,11 @@
 install: # установить зависимости
 	npm ci
 
-brain-games: # запуск игры
+brain-games: # запуск приветствия
 	node bin/brain-games.js
+
+brain-even: # запуск игры even
+	node bin/brain-even.js
 
 publish: # публичность
 	npm publish --dry-run
---
+++ src/games/even.js
@@ -0,0 +1,60 @@
+/* eslint-disable linebreak-style */
+import readlineSync from 'readline-sync';
+
+//const even = ()
+export default () => {
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  const randomNumber1 = Math.floor(Math.random() * 11);
+  console.log(randomNumber1);
+  const answer = readlineSync.question('Your answer: ');
+  if (randomNumber1 % 2 === 0 && answer === 'yes') {
+    console.log('Correct!');
+  }
+  if (randomNumber1 % 2 === 0 && answer !== 'yes') {
+    console.log("'no' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, Bill!");
+    return false;
+  }
+  if (randomNumber1 % 2 !== 0 && answer !== 'no') {
+    console.log("'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, Bill!");
+    return false;
+  }
+  if (randomNumber1 % 2 !== 0 && answer === 'no') {
+    console.log('Correct!');
+  }
+  const randomNumber2 = Math.floor(Math.random() * 11);
+  console.log(randomNumber2);
+  const answer2 = readlineSync.question('Your answer: ');
+  if (randomNumber2 % 2 === 0 && answer2 === 'yes') {
+    console.log('Correct!');
+  }
+  if (randomNumber2 % 2 === 0 && answer2 !== 'yes') {
+    console.log("'no' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, Bill!");
+    return false;
+  }
+  if (randomNumber2 % 2 !== 0 && answer2 !== 'no') {
+    console.log("'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, Bill!");
+    return false;
+  }
+  if (randomNumber2 % 2 !== 0 && answer2 === 'no') {
+    console.log('Correct!');
+  }
+
+  const randomNumber3 = Math.floor(Math.random() * 11);
+  console.log(randomNumber3);
+  const answer3 = readlineSync.question('Your answer: ');
+  if (randomNumber3 % 2 === 0 && answer3 === 'yes') {
+    console.log('Correct!');
+  }
+  if (randomNumber3 % 2 === 0 && answer3 !== 'yes') {
+    console.log("'no' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, Bill!");
+    return false;
+  }
+  if (randomNumber3 % 2 !== 0 && answer3 !== 'no') {
+    console.log("'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, Bill!");
+    return false;
+  }
+  if (randomNumber3 % 2 !== 0 && answer3 === 'no') {
+    console.log('Correct!');
+  }
+  console.log('Congratulations, Bill!');
+};
