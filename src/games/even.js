6318da52c73149b45cb53d5a255d6428cf7b95a6
Yuliya Ilina <shubinayv@ya.ru>
2021-07-23 18:42:46 +0300

    step 5 add game and asciinema

--- src/games/even.js
+++ src/games/even.js
@@ -1,8 +1,11 @@
 /* eslint-disable linebreak-style */
 import readlineSync from 'readline-sync';
 
-//const even = ()
+// const even = ()
 export default () => {
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
   const randomNumber1 = Math.floor(Math.random() * 11);
   console.log(randomNumber1);
@@ -11,11 +14,11 @@ export default () => {
     console.log('Correct!');
   }
   if (randomNumber1 % 2 === 0 && answer !== 'yes') {
-    console.log("'no' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, Bill!");
+    console.log(`'no' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, ${name}!`);
     return false;
   }
   if (randomNumber1 % 2 !== 0 && answer !== 'no') {
-    console.log("'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, Bill!");
+    console.log(`'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${name}!`);
     return false;
   }
   if (randomNumber1 % 2 !== 0 && answer === 'no') {
@@ -28,11 +31,11 @@ export default () => {
     console.log('Correct!');
   }
   if (randomNumber2 % 2 === 0 && answer2 !== 'yes') {
-    console.log("'no' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, Bill!");
+    console.log(`'no' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, ${name}!`);
     return false;
   }
   if (randomNumber2 % 2 !== 0 && answer2 !== 'no') {
-    console.log("'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, Bill!");
+    console.log(`'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${name}!`);
     return false;
   }
   if (randomNumber2 % 2 !== 0 && answer2 === 'no') {
@@ -46,15 +49,15 @@ export default () => {
     console.log('Correct!');
   }
   if (randomNumber3 % 2 === 0 && answer3 !== 'yes') {
-    console.log("'no' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, Bill!");
+    console.log(`'no' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, ${name}!`);
     return false;
   }
   if (randomNumber3 % 2 !== 0 && answer3 !== 'no') {
-    console.log("'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, Bill!");
+    console.log(`'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${name}!`);
     return false;
   }
   if (randomNumber3 % 2 !== 0 && answer3 === 'no') {
     console.log('Correct!');
   }
-  console.log('Congratulations, Bill!');
+  console.log(`Congratulations, ${name}!`);
 };
